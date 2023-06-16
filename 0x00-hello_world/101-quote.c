4f8a3ce6cacf76c188c38a49902b8d2ea88a12a3
Rafiw adams <rafiwadams004@gmail.com>
2023-06-15 18:24:00 -0700

    quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,10 +1,12 @@
 #include <unistd.h>
+
 /**
- * main -A C Program that print a line to the standard error
- * Return 1 (success)
+ * main - Entry point
+ *
+ * Return: Always 1 (Error code)
  */
 int main(void)
 {
-	fput("and that piece of art is usefu\" - Dora Korpar, 2015-10-19\r", stdout);
-return (1);
+    write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+    return (1);
 }
