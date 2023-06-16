51b86b83379afe0273685b32d1ac067cb2708767
Rafiw adams <rafiwadams004@gmail.com>
2023-06-15 18:13:41 -0700

    101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+ * main -A C Program that print a line to the standard error
+ * Return 1 (success)
+ */
+int main(void)
+{
+	fput("and that piece of art is usefu\" - Dora Korpar, 2015-10-19\r", stdout);
+return (1);
+}
