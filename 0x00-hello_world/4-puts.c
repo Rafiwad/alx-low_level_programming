65cdc6d6ae83fb08d6aed055cd674494b957818a
Rafiw adams <rafiwadams004@gmail.com>
2023-06-15 17:13:22 -0700

    put

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,3 @@
-#include <stdio.h>
-
 /**
  * main - Entry point of the program
  *
@@ -7,7 +5,6 @@
  */
 int main(void)
 {
-    puts("\"Programming is like building a multilingual puzzle");
-    return (0);
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
-
