2a6f216d5cd9babc1693c8f09c09f23004178217
rafiw adams <rafiwadams004@gmail.com>
2023-07-10 17:34:00 -0700

    0x0A. C - argc, argv

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,36 +1,15 @@
 #include <stdio.h>
-#include "main.h"
 
 /**
  * main - Entry point
  * @argc: The number of command line arguments
  * @argv: An array of strings representing the command line arguments
  *
- * Return: 0 (Success)
+ * Return: Always 0
  */
-int main(int argc, char *argv[])
+int main(__attribute__((unused)) int argc, char *argv[])
 {
-	int i;
-
-	(void)argc; /* Unused parameter */
-
-	for (i = 0; argv[0][i] != '\0'; i++)
-	{
-	_putchar(argv[0][i]);
-	}
-
-	_putchar('\n');
+	printf("%s\n", argv[0]);
 
 	return (0);
-}
-
-/**
- * _putchar - Writes a character to the standard output
- * @c: The character to be written
- *
- * Return: On success, 1. On error, -1 is returned.
- */
-int _putchar(char c)
-{
-	return (putchar(c));
 }
--- 0x0A-argc_argv/main.h
+++ 0x0A-argc_argv/main.h
@@ -9,5 +9,6 @@ int main(int argc, char *argv[]);
 int get_change(int cents);
 int main(int argc, char *argv[]);
 int calculate_coins(int cents);
+int main(__attribute__((unused)) int argc, char *argv[]);
 
 #endif /* MAIN_H */
